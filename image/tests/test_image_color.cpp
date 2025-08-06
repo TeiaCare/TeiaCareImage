093a2384995c6ad2dec1eb1f900329b949eb133d
Stefano Lusardi <stefano.lusardi@teiacare.com>
2025-08-06 15:45:13 +0200

    Restored missing rgbc bgra functions unit tests

--- image/tests/test_image_color.cpp
+++ image/tests/test_image_color.cpp
@@ -126,6 +126,28 @@ TEST_F(color_test, to_bgr_conversion)
     EXPECT_EQ(bgr_array[2], 100); // r
 }
 
+TEST_F(color_test, to_rgba_conversion)
+{
+    tc::img::color c(100, 150, 200, 128);
+    auto rgba_array = c.to_rgba();
+    EXPECT_EQ(rgba_array.size(), 4);
+    EXPECT_EQ(rgba_array[0], 100); // r
+    EXPECT_EQ(rgba_array[1], 150); // g
+    EXPECT_EQ(rgba_array[2], 200); // b
+    EXPECT_EQ(rgba_array[3], 128); // a
+}
+
+TEST_F(color_test, to_bgra_conversion)
+{
+    tc::img::color c(100, 150, 200, 128);
+    auto bgra_array = c.to_bgra();
+    EXPECT_EQ(bgra_array.size(), 4);
+    EXPECT_EQ(bgra_array[0], 200); // b
+    EXPECT_EQ(bgra_array[1], 150); // g
+    EXPECT_EQ(bgra_array[2], 100); // r
+    EXPECT_EQ(bgra_array[3], 128); // a
+}
+
 // Test equality operators
 TEST_F(color_test, equality_operator)
 {
@@ -161,6 +183,89 @@ TEST_F(color_test, boundary_values)
     EXPECT_EQ(c_max.g, 255);
     EXPECT_EQ(c_max.b, 255);
     EXPECT_EQ(c_max.a, 255);
+}
+
+// Test rgba/bgra conversions with boundary values
+TEST_F(color_test, rgba_bgra_conversion_boundary_values)
+{
+    // Test minimum values
+    tc::img::color c_min(0, 0, 0, 0);
+    auto rgba_min = c_min.to_rgba();
+    auto bgra_min = c_min.to_bgra();
+
+    EXPECT_EQ(rgba_min[0], 0); // r
+    EXPECT_EQ(rgba_min[1], 0); // g
+    EXPECT_EQ(rgba_min[2], 0); // b
+    EXPECT_EQ(rgba_min[3], 0); // a
+
+    EXPECT_EQ(bgra_min[0], 0); // b
+    EXPECT_EQ(bgra_min[1], 0); // g
+    EXPECT_EQ(bgra_min[2], 0); // r
+    EXPECT_EQ(bgra_min[3], 0); // a
+
+    // Test maximum values
+    tc::img::color c_max(255, 255, 255, 255);
+    auto rgba_max = c_max.to_rgba();
+    auto bgra_max = c_max.to_bgra();
+
+    EXPECT_EQ(rgba_max[0], 255); // r
+    EXPECT_EQ(rgba_max[1], 255); // g
+    EXPECT_EQ(rgba_max[2], 255); // b
+    EXPECT_EQ(rgba_max[3], 255); // a
+
+    EXPECT_EQ(bgra_max[0], 255); // b
+    EXPECT_EQ(bgra_max[1], 255); // g
+    EXPECT_EQ(bgra_max[2], 255); // r
+    EXPECT_EQ(bgra_max[3], 255); // a
+}
+
+// Test rgba/bgra conversions with static colors
+TEST_F(color_test, rgba_bgra_conversion_static_colors)
+{
+    // Test red color
+    auto red_color = tc::img::color::red();
+    auto red_rgba = red_color.to_rgba();
+    auto red_bgra = red_color.to_bgra();
+
+    EXPECT_EQ(red_rgba[0], 255); // r
+    EXPECT_EQ(red_rgba[1], 0);   // g
+    EXPECT_EQ(red_rgba[2], 0);   // b
+    EXPECT_EQ(red_rgba[3], 255); // a
+
+    EXPECT_EQ(red_bgra[0], 0);   // b
+    EXPECT_EQ(red_bgra[1], 0);   // g
+    EXPECT_EQ(red_bgra[2], 255); // r
+    EXPECT_EQ(red_bgra[3], 255); // a
+
+    // Test green color
+    auto green_color = tc::img::color::green();
+    auto green_rgba = green_color.to_rgba();
+    auto green_bgra = green_color.to_bgra();
+
+    EXPECT_EQ(green_rgba[0], 0);   // r
+    EXPECT_EQ(green_rgba[1], 255); // g
+    EXPECT_EQ(green_rgba[2], 0);   // b
+    EXPECT_EQ(green_rgba[3], 255); // a
+
+    EXPECT_EQ(green_bgra[0], 0);   // b
+    EXPECT_EQ(green_bgra[1], 255); // g
+    EXPECT_EQ(green_bgra[2], 0);   // r
+    EXPECT_EQ(green_bgra[3], 255); // a
+
+    // Test blue color
+    auto blue_color = tc::img::color::blue();
+    auto blue_rgba = blue_color.to_rgba();
+    auto blue_bgra = blue_color.to_bgra();
+
+    EXPECT_EQ(blue_rgba[0], 0);   // r
+    EXPECT_EQ(blue_rgba[1], 0);   // g
+    EXPECT_EQ(blue_rgba[2], 255); // b
+    EXPECT_EQ(blue_rgba[3], 255); // a
+
+    EXPECT_EQ(blue_bgra[0], 255); // b
+    EXPECT_EQ(blue_bgra[1], 0);   // g
+    EXPECT_EQ(blue_bgra[2], 0);   // r
+    EXPECT_EQ(blue_bgra[3], 255); // a
 }
 
 }
